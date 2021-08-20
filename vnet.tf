c2f99c9bf5b8b050194c62515f042fcd59441228
Srividya Ananthasubramoney <san0031@gmail.com>
2021-08-20 18:45:49 +0000

    Added vnet module

---
+++ vnet.tf
@@ -0,0 +1,6 @@
+module "network" {
+  source  = "app.terraform.io/SrividyaAnanthasubramoney-training/network/azurerm"
+  version = "3.0.1"
+    resource_group_name = "${var.prefix}-workshop"
+  # insert required variables here
+}
